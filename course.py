dc9492f6029512eb6f75c7fd15e947f6579dd9d1
Utsav-83 <utsav8391@gmail.com>
2026-01-08 01:30:10 +0530

    Update course.py

--- course.py
+++ course.py
@@ -1,4 +1,17 @@
-def process_course_data(learner_name, course, duration, scores):
+def process_course_data(args):
+    # Defaults
+    learner_name = "Aarav"
+    course = "DevOps Fundamentals"
+    duration = 12
+    scores = [82, 88, 90]
+
+    # Custom CLI args
+    if len(args) > 1:
+        learner_name = args[1]
+        course = args[2]
+        duration = int(args[3])
+        scores = list(map(int, args[4:]))
+
     average = sum(scores) / len(scores)
 
     if average >= 85:
@@ -9,7 +22,7 @@ def process_course_data(learner_name, course, duration, scores):
         status = "Fail"
 
     return {
-        "learner_name": learner_name,
+        "learner": learner_name,
         "course": course,
         "duration": duration,
         "scores": scores,
@@ -19,14 +32,10 @@ def process_course_data(learner_name, course, duration, scores):
 
 
 if __name__ == "__main__":
-    data = process_course_data(
-        "Aarav",
-        "DevOps Fundamentals",
-        12,
-        [82, 88, 90]
-    )
+    import sys
+    data = process_course_data(sys.argv)
 
-    print("Learner Name:", data["learner_name"])
+    print("Learner Name:", data["learner"])
     print("Course:", data["course"])
     print("Duration (weeks):", data["duration"])
     print("Assessment Scores:", *data["scores"])
